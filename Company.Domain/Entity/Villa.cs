1bfbe5ca1caff63faf887d6d53a544a6f7b2ee05
elnaz <ghofrani.elnaz@gmail.com>
2025-04-02 15:56:54 +0100

    Configuration of Connect to DB

---
+++ Company.Domain/Entity/Villa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Domain.Entity
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        //log
+        public DateTime? CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+    }
+}
