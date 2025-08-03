5be838027acc4b29835b515bac56fb9c0ff3376b
Aynaz ghofrani <ghofrani.ainaz@gmail.com>
2025-08-03 23:09:38 +0100

    Migration - Connect to Localdb and creating Database

---
+++ Company.Infrastructure/Migrations/20250803205917_InitialCreate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Company.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreateDate", "UpdateDate" },
+                values: new object[] { new DateTime(2025, 8, 3, 21, 59, 17, 491, DateTimeKind.Local).AddTicks(845), new DateTime(2025, 8, 3, 20, 59, 17, 491, DateTimeKind.Utc).AddTicks(836) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreateDate", "UpdateDate" },
+                values: new object[] { new DateTime(2025, 8, 3, 21, 59, 17, 491, DateTimeKind.Local).AddTicks(928), new DateTime(2025, 8, 3, 20, 59, 17, 491, DateTimeKind.Utc).AddTicks(926) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreateDate", "UpdateDate" },
+                values: new object[] { new DateTime(2025, 4, 10, 18, 6, 44, 467, DateTimeKind.Local).AddTicks(7546), new DateTime(2025, 4, 10, 17, 6, 44, 467, DateTimeKind.Utc).AddTicks(7540) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreateDate", "UpdateDate" },
+                values: new object[] { new DateTime(2025, 4, 10, 18, 6, 44, 467, DateTimeKind.Local).AddTicks(7612), new DateTime(2025, 4, 10, 17, 6, 44, 467, DateTimeKind.Utc).AddTicks(7610) });
+        }
+    }
+}
