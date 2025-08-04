fa52e51dc6d68c81c14dd2352f5c5cdb72210817
Aynaz ghofrani <ghofrani.ainaz@gmail.com>
2025-08-04 16:00:07 +0100

    GetAll Villa + All Setings

--- VillaPortal/Controllers/VillaController.cs
+++ VillaPortal/Controllers/VillaController.cs
@@ -1,6 +1,29 @@
-﻿namespace VillaPortal.Controllers
+﻿using Company.Application.Interface;
+using Company.Domain.Entity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VillaPortal.Controllers
 {
-    public class VillaController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
     {
+        private readonly IVillaService _villaService;
+        
+        public VillaController( IVillaService villaService) { 
+        
+            _villaService = villaService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var villas = await _villaService.GetVillasAsync();
+            if (villas == null)
+            {
+                return NotFound("Village not found");
+            }
+            return Ok(villas);
+        }
     }
 }
--- VillaPortal/Program.cs
+++ VillaPortal/Program.cs
@@ -1,12 +1,22 @@
+using Company.Application.Interface;
+using Company.Application.Mappings;
+using Company.Application.Services;
+using Company.Infrastructure.Data;
+using Company.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(Options => Options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddScoped<IVillaService, VillaService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddAutoMapper(typeof(MappingProfile));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
